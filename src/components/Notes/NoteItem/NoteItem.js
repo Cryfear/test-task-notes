ae018f006754a2ebf691e8faed3e4457abd20330
Cryfear <vasil.arturok@yandex.ru>
2021-10-28 21:26:12 +0300

    everything

--- src/components/Notes/NoteItem/NoteItem.js
+++ src/components/Notes/NoteItem/NoteItem.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import delete_icon from '../../../assets/images/delete-icon.png';
+import React from "react";
+import delete_icon from "../../../assets/images/delete-icon.png";
 import { deleteNote, openNote } from "../../../App.model";
 
-export const NoteItem = ({name, id}) => {
+export const NoteItem = ({ name, id }) => {
   return (
-    <li onClick={() => openNote({id})} className="notes__list-item">{name}
-      <button onClick={() => deleteNote({id})} className="notes__list-item-delete">
-        <img src={delete_icon} alt="deleteIcon"/>
+    <div className="note-item-wrapper">
+      <li onClick={() => openNote({ id })} className="notes__list-item">
+        {name}
+      </li>
+      <button
+        onClick={() => deleteNote({ id })}
+        className="notes__list-item-delete"
+      >
+        <img src={delete_icon} alt="deleteIcon" />
       </button>
-    </li>
-  )
-}
+    </div>
+  );
+};
